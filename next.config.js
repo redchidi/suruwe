e65afb648dcee1a07889b98dcad2d7e9512c58a7
redchidi <chidi.afulezi@redkola.com>
2026-03-09 11:44:27 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+          remotePatterns: [
+            {
+                      protocol: 'https',
+                      hostname: '*.supabase.co',
+                      pathname: '/storage/v1/object/public/**',
+            },
+                ],
+    },
+};
+
+module.exports = withNextIntl(nextConfig);
+remotePatterns: [
       {
         protocol: 'https',
         hostname: '*.supabase.co',
