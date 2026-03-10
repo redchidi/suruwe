354111069acc0eb311c21f082fdb05345d912424
redchidi <chidi.afulezi@redkola.com>
2026-03-09 21:43:16 -0400

    Fix: remove withNextIntl plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const createNextIntlPlugin = require('next-intl/plugin');
-const withNextIntl = createNextIntlPlugin('./i18n.ts');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -14,4 +11,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
